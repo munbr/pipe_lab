6a2e8ab97913ab55c634339e699ca68994fa24bb
munbr <munbr@bk.ru>
2026-09-08 13:33:40 +0300

    second commit

--- lab.cpp
+++ lab.cpp
@@ -11,20 +11,55 @@ struct PIPE
 
 void create_pipe()
 {
-    PIPE pipe('pipa', 0, 0, 0);
+    PIPE pipe = {};
 }
-PIPE edit_pipe()
+void edit_pipe(PIPE p)
 {
-    pipe.name('pipa');
-
-
-
+    while (true)
+    {
+    cout << "Выберите, что вы хотите отредактировать\n" << "1 - имя трубы\n" << "2 - длину трубы\n" << "3 - диаметр трубы\n" << "4 - в ремонте ли труба" << endl;
+        int x;
+        switch (x)
+        {
+        case 1:
+            cout << "Введите новое имя" << endl;
+            cin >> p.name;
+            break;
+        case 2:
+            cout << "Введите новую длину" << endl;
+            break;
+        case 3:
+            cout << "Введите новый диаметр" << endl;
+            break;
+        case 4:
+            cout << "Выберете 1 - если труба в ремонте, 2 - если труба работоспосбна " << endl;
+            break;
+        
+        default: 
+            break;
+        }
+    }
+    
+    
 }
 
 int main()
 {
-    create_pipe;
-    pipe.name('pipa')l;
+    cout << "Выберите опцию\n" << "1 - создать трубу\n" << endl;
+    int option;
+    cin >> option;
+    switch (option)
+    {
+    case 1:
+        create_pipe;
+        break;
+    
+    default:
+        break;
+    }
+    
+
+    
 
 
 }
