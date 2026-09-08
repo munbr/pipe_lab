e485ac5be64ce9427c11675f5fcd478e9cc4cd9e
munbr <munbr@bk.ru>
2026-09-08 12:56:44 +0300

    fiirst struct

---
+++ lab.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+struct PIPE
+{   
+    string name;
+    double pipe_length;
+    double pipe_diam;
+    bool pipe_tech;
+};
+
+void create_pipe()
+{
+
+}
+
+int main()
+{
+
+
+}
